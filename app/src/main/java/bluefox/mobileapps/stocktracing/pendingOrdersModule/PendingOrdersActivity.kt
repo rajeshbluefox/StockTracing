f52c2672b40845b2a691d5607a7e404cf7acc879
rajeshbluefox <bluefoxmobileapps@gmail.com>
2025-01-13 23:08:59 +0530

    Pending Orders Creation

---
+++ app/src/main/java/bluefox/mobileapps/stocktracing/pendingOrdersModule/PendingOrdersActivity.kt
@@ -0,0 +1,16 @@
+package bluefox.mobileapps.stocktracing.pendingOrdersModule
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import bluefox.mobileapps.stocktracing.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class PendingOrdersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
